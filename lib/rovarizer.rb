60378c097f38eabbc1ec3e9cf5d1dd3e2a70501a
itgsod-andreas-lindell <andreas.lindell@Administrators-MacBook-Pro-169.local>
2015-05-22 14:48:43 +0200

    klar funktionel rovarizer
    gjordes på fritid

--- lib/rovarizer.rb
+++ lib/rovarizer.rb
@@ -1,6 +1,13 @@
-def rovarize(cleartext: )
-  vowels = [a,e,i,o,u,y,å,ä,ö]
-  vowels.each do |vowel|
-    cleartext.next
+def rovarize (cleartext:)
+  new_string = ""
+  konsonanter = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Z", "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"]
+  cleartext.each_char do |bokstav|
+    if konsonanter.include? bokstav
+      new_string += (bokstav + "o" + bokstav)
+    else
+      new_string += bokstav
+    end
   end
+  return new_string
 end
+p rovarize(cleartext: "swag")
